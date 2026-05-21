33f0373b0374edaeb9e5ff57136bfd5fb2a3bbb1
ratchaneechaisua68 <ratchaneechai.sua68@prtc.ac.th>
2026-05-21 14:37:37 +0700

    day-2 connect string

---
+++ string_s.js
@@ -0,0 +1,13 @@
+let a ='JAVA' + 'Script'
+let b ='ECMA'
+let c = 10
+console.log(b + 'Script ' + c );
+let d = 31
+let m = 12
+let y = 2026
+console.log('วันนี้ตรงกับ ' + d + '/' + m + '/' + y );
+var g = 5
+var n = 'สมชาย'
+console.log(`เชิญคุณ ${n} รับยาที่ช่อง ${g} ค่ะ`);
+
+
